79713e1d9d63926199744df98c4c95d1ab6a5df1
Suzume <suzume.logs@gmail.com>
2024-09-12 15:42:55 +0700

    Update

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -4,74 +4,8 @@ import ArrowRightWhiteIcon from '../assets/svgs/arrow-right-white-icon.svg'
 import ArrowRightPrimaryIcon from '../assets/svgs/arrow-right-primary-icon.svg'
 import LocationIcon from '../assets/svgs/location-icon.svg'
 import AsyncIcon from '../assets/svgs/async-icon.svg'
-import Product1Image from '../assets/images/product-1.png'
-import Product2Image from '../assets/images/product-1.png'
-import Product3Image from '../assets/images/product-1.png'
-import Product4Image from '../assets/images/product-1.png'
 import { useState } from 'react'
-
-const products = [
-  {
-    id: 1,
-    image: Product1Image,
-    title: 'Yanmar Compact Excavators Summarized - 2017',
-    location: 'Hà nội',
-    year: '2017',
-    usage: '1000 giờ',
-    deliveryTime: '15 -20 ngày',
-    oldPrice: '2.500.000.000đ',
-    newPrice: '2.500.000.000đ',
-    expiry: '3d:5h:60m'
-  },
-  {
-    id: 2,
-    image: Product2Image,
-    title: 'Yanmar Compact Excavators Summarized - 2017',
-    location: 'Hà nội',
-    year: '2017',
-    usage: '1000 giờ',
-    deliveryTime: '15 -20 ngày',
-    oldPrice: '2.500.000.000đ',
-    newPrice: '2.500.000.000đ',
-    expiry: '3d:5h:60m'
-  },
-  {
-    id: 3,
-    image: Product3Image,
-    title: 'Yanmar Compact Excavators Summarized - 2017',
-    location: 'Hà nội',
-    year: '2017',
-    usage: '1000 giờ',
-    deliveryTime: '15 -20 ngày',
-    oldPrice: '2.500.000.000đ',
-    newPrice: '2.500.000.000đ',
-    expiry: '3d:5h:60m'
-  },
-  {
-    id: 4,
-    image: Product4Image,
-    title: 'Yanmar Compact Excavators Summarized - 2017',
-    location: 'Hà nội',
-    year: '2017',
-    usage: '1000 giờ',
-    deliveryTime: '15 -20 ngày',
-    oldPrice: '2.500.000.000đ',
-    newPrice: '2.500.000.000đ',
-    expiry: '3d:5h:60m'
-  },
-  {
-    id: 5,
-    image: Product4Image,
-    title: 'Yanmar Compact Excavators Summarized - 2017',
-    location: 'Hà nội',
-    year: '2017',
-    usage: '1000 giờ',
-    deliveryTime: '15 -20 ngày',
-    oldPrice: '2.500.000.000đ',
-    newPrice: '2.500.000.000đ',
-    expiry: '3d:5h:60m'
-  }
-]
+import { products } from '~/constants'
 
 const Product = () => {
   const [currentIndex, setCurrentIndex] = useState(0)
---
+++ src/constants/index.ts
@@ -0,0 +1,67 @@
+import Product1Image from '../assets/images/product-1.png'
+import Product2Image from '../assets/images/product-1.png'
+import Product3Image from '../assets/images/product-1.png'
+import Product4Image from '../assets/images/product-1.png'
+
+export const products = [
+  {
+    id: 1,
+    image: Product1Image,
+    title: 'Yanmar Compact Excavators Summarized - 2017',
+    location: 'Hà nội',
+    year: '2017',
+    usage: '1000 giờ',
+    deliveryTime: '15 -20 ngày',
+    oldPrice: '2.500.000.000đ',
+    newPrice: '2.500.000.000đ',
+    expiry: '3d:5h:60m'
+  },
+  {
+    id: 2,
+    image: Product2Image,
+    title: 'Yanmar Compact Excavators Summarized - 2017',
+    location: 'Hà nội',
+    year: '2017',
+    usage: '1000 giờ',
+    deliveryTime: '15 -20 ngày',
+    oldPrice: '2.500.000.000đ',
+    newPrice: '2.500.000.000đ',
+    expiry: '3d:5h:60m'
+  },
+  {
+    id: 3,
+    image: Product3Image,
+    title: 'Yanmar Compact Excavators Summarized - 2017',
+    location: 'Hà nội',
+    year: '2017',
+    usage: '1000 giờ',
+    deliveryTime: '15 -20 ngày',
+    oldPrice: '2.500.000.000đ',
+    newPrice: '2.500.000.000đ',
+    expiry: '3d:5h:60m'
+  },
+  {
+    id: 4,
+    image: Product4Image,
+    title: 'Yanmar Compact Excavators Summarized - 2017',
+    location: 'Hà nội',
+    year: '2017',
+    usage: '1000 giờ',
+    deliveryTime: '15 -20 ngày',
+    oldPrice: '2.500.000.000đ',
+    newPrice: '2.500.000.000đ',
+    expiry: '3d:5h:60m'
+  },
+  {
+    id: 5,
+    image: Product4Image,
+    title: 'Yanmar Compact Excavators Summarized - 2017',
+    location: 'Hà nội',
+    year: '2017',
+    usage: '1000 giờ',
+    deliveryTime: '15 -20 ngày',
+    oldPrice: '2.500.000.000đ',
+    newPrice: '2.500.000.000đ',
+    expiry: '3d:5h:60m'
+  }
+]
