c5c3b8111a584b944492f5e81d0ef4f7bd37342d
CuongThieu <cuongtt@solashi.com>
2024-09-13 11:34:12 +0700

    Update

--- src/components/common/AutoSlider.tsx
+++ src/components/common/AutoSlider.tsx
@@ -85,8 +85,8 @@ import FinalMMasterImage from '../../assets/images/finals/final-master.png'
 const AutoSlider = () => {
   return (
     <div className='relative w-full mx-auto'>
-      <img src={FinalMMasterImage} alt='Slide' className='object-fill sm:object-contain w-full h-[250px] sm:h-full' />
-      <button className='absolute left-[50px] bottom-[70px] px-4 py-[11px] w-full sm:w-[165px] bg-[#FFA21A] text-[#2C2A29] rounded-lg shadow-md transform focus:outline-none focus:ring-2 focus:ring-[#FFA21A] opacity-100 font-semibold hover:opacity-80 transition-opacity duration-300 mt-2 sm:mt-0 hidden sm:block'>
+      <img src={FinalMMasterImage} alt='Slide' className='object-fill md:object-contain w-full h-[250px] md:h-full' />
+      <button className='absolute left-[10px] md:left-[60px] bottom-[20px] md:bottom-[70px] md:px-4 md:py-[11px] w-[113px] md:w-[165px] bg-[#FFA21A] text-[#2C2A29] rounded-[4.6px]  md:rounded-lg shadow-md transform focus:outline-none focus:ring-2 focus:ring-[#FFA21A] opacity-100 font-semibold hover:opacity-80 transition-opacity duration-300 mt-2 md:mt-0 text-[9px] md:text-[16px] h-[27px] md:h-[46px]'>
         Đăng ký ngay
       </button>
     </div>
