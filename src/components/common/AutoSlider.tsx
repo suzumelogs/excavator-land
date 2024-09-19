26e21268d29254323578cda0db56792afad5f0d0
CuongThieu <cuongtt@solashi.com>
2024-09-19 16:32:56 +0700

    Update

--- src/components/common/AutoSlider.tsx
+++ src/components/common/AutoSlider.tsx
@@ -1,4 +1,5 @@
-import FinalMMasterImage from '../../assets/images/finals/final-master.png'
+import FinalMasterImage from '../../assets/images/finals/final-master.png'
+import FinalMasterMobileImage from '../../assets/images/finals/final-master-mobile.png'
 
 const AutoSlider = () => {
   const handleSubmit = () => {
@@ -10,7 +11,16 @@ const AutoSlider = () => {
 
   return (
     <div className='relative w-full mx-auto'>
-      <img src={FinalMMasterImage} alt='Slide' className='object-fill md:object-contain w-full h-[250px] md:h-full' />
+      <img
+        src={FinalMasterImage}
+        alt='Slide'
+        className='object-fill md:object-contain w-full h-[250px] md:h-full hidden md:block'
+      />
+      <img
+        src={FinalMasterMobileImage}
+        alt='Slide'
+        className='object-fill md:object-contain w-full h-[250px] md:h-full block md:hidden'
+      />
       <button
         onClick={handleSubmit}
         className='absolute left-[10px] md:left-[60px] bottom-[20px] md:bottom-[70px] md:px-4 md:py-[11px] w-[113px] md:w-[165px] bg-[#FFA21A] text-[#2C2A29] rounded-[4.6px]  md:rounded-lg shadow-md transform focus:outline-none focus:ring-2 focus:ring-[#FFA21A] opacity-100 font-semibold hover:opacity-80 transition-opacity duration-300 mt-2 md:mt-0 text-[9px] md:text-[16px] h-[27px] md:h-[46px]'
